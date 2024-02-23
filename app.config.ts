99f0fa6e6f9dfb470ffaeddf0016df38154b8b99
sambooora <richiesambora9029@gmail.com>
2024-02-23 14:57:18 +0700

    add: vcalendar

---
+++ app.config.ts
@@ -0,0 +1,15 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'purple',
+    gray: 'zinc',
+    notifications: {
+      // Show toasts at the top right of the screen
+      position: 'bottom-0 right-0',
+    },
+    avatar: {
+      size: {
+        custom: 'h-44 w-44 text-9xl',
+      },
+    },
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,49 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  app: {
+    layoutTransition: {
+      name: 'layout',
+      mode: 'out-in',
+    },
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in',
+    },
+  },
+  ui: {
+    global: true,
+    icons: ['heroicons'],
+  },
+  imports: {
+    dirs: ['composables', 'stores', 'utils'],
+    presets: [
+      {
+        from: 'zod',
+        imports: [
+          'z',
+          {
+            name: 'z',
+            as: 'zType',
+            type: true,
+          },
+        ],
+      },
+      {
+        from: '#ui/types',
+        imports: ['FormSubmitEvent'],
+        type: true,
+      },
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      app: {
+        name: 'DApp',
+        description: 'App for new starter Dapp',
+        twitterHandle: '@sambooora',
+        appVersion: process.env.APP_VER || '0.0.0',
+      },
+    },  },
 })
